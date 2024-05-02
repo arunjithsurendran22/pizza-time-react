16dd9b947e6c2378ae2276c63d684cdcbcee5170
arunjith surendran <arunjithsurendran22@gmail.com>
2024-05-02 16:35:26 +0530

    new one

--- src/cards/CardFive.jsx
+++ src/cards/CardFive.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const CardFive = () => {
   return (
-    <div className="mt-10 md:mt-0 w-full lg:w-auto h-auto xl:w-4/12 lg:h-[510px] bg-gradient-to-r from-BlueBackground to-blue-400 rounded-3xl">
+    <div className="mt-10 xl:mt-0 w-full lg:w-auto h-auto xl:w-4/12 xl:h-[510px] bg-gradient-to-r from-BlueBackground to-blue-400 rounded-3xl">
       <div className="h-full text-center lg:text-left">
         <div className="text-xl lg:text-4xl font-bold p-5 md:p-12">
           <h1>Create and save your prompts</h1>
